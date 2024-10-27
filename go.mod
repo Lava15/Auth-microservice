15d433c3dc544f2eaeb86d32c218739e44cbe907
LA <no>
2024-10-27 16:25:01 +0500

    in p

--- go.mod
+++ go.mod
@@ -3,5 +3,15 @@ module github.com/lava15/Auth-microservice
 go 1.22.4
 
 require (
-	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	golang.org/x/crypto v0.28.0
+	google.golang.org/grpc v1.67.1
+	google.golang.org/protobuf v1.35.1
+)
+
+require (
+	golang.org/x/net v0.28.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
 )
--- internal/auth/service.go
+++ internal/auth/service.go
@@ -1,1 +1,36 @@
 package auth
+
+import (
+	"context"
+	"errors"
+	"github.com/lava15/Auth-microservice/pkg/jwt"
+	"golang.org/x/crypto/bcrypt"
+	"sync"
+)
+
+type AuthService struct {
+	users      map[string]string
+	mu         sync.RWMutex
+	jwtManager *jwt.JWTManager
+}
+
+func NewAuthService(jwtManager *jwt.JWTManager) *AuthService {
+	return &AuthService{
+		users:      make(map[string]string),
+		jwtManager: jwtManager,
+	}
+}
+
+func (s *AuthService) Register(ctx context.Context, req *RegisterRequest) (*RegisterResponse, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if _, exists := s.users[req.Username]; exists {
+		return nil, errors.New("user already exists")
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password).bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	s.users[req.Username] = string(hashedPassword)
+	return &RegisterResponse{Message: "User registered!"}
+}
